46cf912bb0f8ce9c63e83c324a8b276f5e31e51f
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-01-22 18:46:13 -0500

    Added loaders, actions, and dynamic routes to finish off the application

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import NewPost from './routes/NewPost';
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails';
 import RootLayout from './routes/RootLayout';
 import './index.css';
-import Posts from './routes/Posts';
+import Posts, { loader as postsLoader } from './routes/Posts';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,11 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Posts />,
-        children: [{ path: '/create-post', element: <NewPost /> }],
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails />, loader: postDetailsLoader },
+        ],
       },
     ],
   },
