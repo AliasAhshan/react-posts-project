f85171d186a4bf8b38c4c6b2c51dd99e5ecdc0eb
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-01-22 18:30:34 -0500

    Added routing and reorganized files

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
 
 import Post from './Post';
-import NewPost from './NewPost';
-import Modal from './Modal';
 
 import classes from './PostsList.module.css';
 
-function PostsList({ isPosting, onStopPosting }) {
+function PostsList() {
   // fetch('http://localhost:8080/posts')
   //   .then((response) => response.json())
   //   .then((data) => {
@@ -40,11 +38,6 @@ function PostsList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
